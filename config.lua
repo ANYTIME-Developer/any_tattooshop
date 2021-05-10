7e6af4b1921eeaffb352a67968c7fb9e60442d39
ANYTIME-Developer <blogdelibros1@gmail.com>
2021-05-10 15:20:09 +0200

    1.0

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config              = {}
+
+Config.DrawDistance = 25.0
+Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
+Config.Color        = {r = 255, g = 0, b = 0}
+Config.Type         = 1
+
+Config.Locale       = 'es'
+
+Config.PriceRemoveTattoo = 700
+
+Config.Zones = {
+	vector3(1322.6, -1651.9, 51.2),
+	vector3(-1153.6, -1425.6, 4.0),
+	vector3(322.1, 180.4, 102.66),
+	vector3(-3170.0, 1075.0, 19.88),
+	vector3(1864.6, 3747.7, 32.07),
+	vector3(-293.7,  6200.0,  30.53)
+}
---
+++ locales/es.lua
@@ -0,0 +1,12 @@
+Locales['es'] = {
+  ['tattoo_shop_prompt'] = 'Presiona ~INPUT_PICKUP~ para acceder a la ~y~tienda de tatuajes~s~.',
+  ['money_amount']       = '<span style="color:green;">%s€</span>',
+  ['part']               = 'parte %s',
+  ['go_back_to_menu']    = '< Atras',
+  ['tattoo_item']        = 'tattoo %s - %s',
+  ['tattoos']            = 'tatuajes',
+  ['tattoo_shop']        = 'Tienda de Tatuajes',
+  ['bought_tattoo']      = 'has comprado un ~y~tattoo~s~ por ~r~%s€~s~',
+  ['remove_tattoo']      = 'Te has quitado un ~y~tattoo~s~ por ~r~%s€~s~',
+  ['not_enough_money']   = 'no tienes ~r~suficiente dinero~s~ para ese tattoo! Te faltan ~r~%s€~s~'
+}
